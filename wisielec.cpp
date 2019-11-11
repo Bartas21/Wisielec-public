19542b778e2bd4c640d0eef9535b2149dfa78d41
Bartas21 <56482972+Bartas21@users.noreply.github.com>
2019-11-11 21:48:10 +0100

    Update wisielec.cpp

--- wisielec.cpp
+++ wisielec.cpp
@@ -1,16 +1,19 @@
-#include "wisielec.h"
 #include "ui_wisielec.h"
-#include <QFile>
-#include <QDebug>
-#include <QBuffer>
-#include <QRandomGenerator>
+#include "wisielec.h"
+#include "qbitmap.h"
 
 Wisielec::Wisielec(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Wisielec)
 {
+
     ui->setupUi(this);
 
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap("C:/Users/Tina/Desktop/wisielec/obraz/0.png");
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 Wisielec::~Wisielec()
@@ -18,192 +21,288 @@ Wisielec::~Wisielec()
     delete ui;
 }
 
-void Draw(int score = 0)
-{
-    if (score >= 1)
-        QPixmap pix("1.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score >= 2)
-        QPixmap pix("2.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score >= 3)
-        QPixmap pix("3.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score == 4)
-        QPixmap pix("4.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score == 5)
-        QPixmap pix("5.png ", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score >= 6)
-        QPixmap pix("6.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-    if (score >= 7)
-        QPixmap pix("7.png", false, false);
-    else
-        QPixmap pix("", false, false);
-
-}
-
-void Wisielec::chooseRandomWord()
-{
-    QMutexLocker locker(&m_lock);
-    if (m_wordList.isEmpty())
-        return;
-
-    m_word = m_wordList.at(QRandomGenerator::global()->bounded(m_wordList.size()));
-    emit wordChanged();
-}
-
-void Wisielec::initWordList()
-{
-    QMutexLocker locker(&m_lock);
-    QFile file(":/stolice.txt");
-    if (file.open(QIODevice::ReadOnly)) {
-        QByteArray allData = file.readAll();
-        QBuffer buffer(&allData);
-        if (!buffer.open(QIODevice::ReadOnly))
-            qFatal("Couldn't open buffer for reading!");
-
-        while (!buffer.atEnd()) {
-            QByteArray ba = buffer.readLine().trimmed().toUpper();
-            if (!ba.isEmpty() && ba.length() < 10)
-                m_wordList.append(QString::fromLatin1(ba));
-        }
-    }
-
-    chooseRandomWord();
-}
 
 void Wisielec::on_A_clicked()
 {
     ui->label->setText("A");
+    manager.chek('A');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
+
 }
 
 void Wisielec::on_B_clicked()
 {
     ui->label->setText("B");
+    manager.chek('B');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_C_clicked()
 {
     ui->label->setText("C");
-}
+    manager.chek('C');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
 
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
+}
 
 void Wisielec::on_D_clicked()
 {
     ui->label->setText("D");
+    manager.chek('D');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_E_clicked()
 {
     ui->label->setText("E");
+    manager.chek('E');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_F_clicked()
 {
     ui->label->setText("F");
+    manager.chek('F');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_G_clicked()
 {
     ui->label->setText("G");
+    manager.chek('G');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_H_clicked()
 {
     ui->label->setText("H");
+    manager.chek('H');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_I_clicked()
 {
     ui->label->setText("I");
+    manager.chek('I');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_J_clicked()
 {
     ui->label->setText("J");
+    manager.chek('J');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_K_clicked()
 {
     ui->label->setText("K");
+    manager.chek('K');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_L_clicked()
 {
     ui->label->setText("L");
+    manager.chek('L');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_M_clicked()
 {
     ui->label->setText("M");
+    manager.chek('M');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_N_clicked()
 {
     ui->label->setText("N");
+    manager.chek('N');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_O_clicked()
 {
     ui->label->setText("O");
+    manager.chek('O');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_P_clicked()
 {
     ui->label->setText("P");
+    manager.chek('P');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_R_clicked()
 {
     ui->label->setText("R");
+    manager.chek('R');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_S_clicked()
 {
     ui->label->setText("S");
+    manager.chek('S');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_T_clicked()
 {
     ui->label->setText("T");
+    manager.chek('T');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_U_clicked()
 {
     ui->label->setText("U");
+    manager.chek('U');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_W_clicked()
 {
     ui->label->setText("W");
+    manager.chek('W');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_X_clicked()
 {
     ui->label->setText("X");
+    manager.chek('X');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_Y_clicked()
 {
     ui->label->setText("Y");
+    manager.chek('Y');
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
 
 void Wisielec::on_Z_clicked()
 {
     ui->label->setText("Z");
+    manager.chek('Z');
+
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap(manager.pic());
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
 }
+
+
+void Wisielec::on_RESET_clicked()
+{
+    manager.new_password();
+
+    ui->label_2->setText(QString::fromStdString(manager.flor_table));
+
+    QPixmap pixmap("C:/Users/Tina/Desktop/wisielec/obraz/0.png");
+    ui->label_pic->setPixmap(pixmap);
+    ui->label_pic->setMask(pixmap.mask());
+}
+
+
+
